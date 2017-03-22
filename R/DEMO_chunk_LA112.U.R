f24f07363c7304f01f84037cf5e292a5457fa5ac
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-03-22 10:48:32 -0400

    Translated L112.U_DEMO
    Note found incorrect behavior in step 3; see fix in code.

--- R/DEMO_chunk_LA112.U.R
+++ R/DEMO_chunk_LA112.U.R
@@ -1,6 +1,6 @@
-#' module_energy_LA112.U_DEMO_DISABLED
+#' module_energy_LA112.U_DEMO
 #'
-#' Briefly describe what this chunk does.
+#' Build the GCAM uranium energy resource curves.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
@@ -8,14 +8,15 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{L112.RsrcCurves_Mt_R_U}. The corresponding file in the
 #' original data system was \code{LA112.U.R} (energy level1).
-#' @details Describe in detail what this chunk does.
+#' @details Currently not built up from inventory data; just using GCAM 3.0 values
+#' These were not disaggregated to regions in GCAM 3.0.
 #' @importFrom assertthat assert_that
 #' @importFrom tibble tibble
 #' @import dplyr
 #' @importFrom tidyr gather spread
-#' @author Author name(s)
+#' @author BBL March 2017
 #' @export
-module_energy_LA112.U_DEMO_DISABLED <- function(command, ...) {
+module_energy_LA112.U_DEMO <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "energy/A12.U_curves",
@@ -34,24 +35,43 @@ module_energy_LA112.U_DEMO_DISABLED <- function(command, ...) {
     # that we won't be called until this is available
     L113.RsrcCurves_EJ_R_MSW <- get_data(all_data, "L113.RsrcCurves_EJ_R_MSW")
 
-    # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    # ===================================================
+    # 2a. Uranium supply curves
+    # Currently not built up from inventory data; just using GCAM 3.0 values
+    #These were not disaggregated to regions in GCAM 3.0. Keeping this convention for now.
+    #    printlog( "NOTE: Assigning global uranium supply curve to GCAM_region_ID 1")
+    tibble(GCAM_region_ID = 1,
+           resource = A12.U_curves$resource,
+           subresource = A12.U_curves$subresource,
+           grade = A12.U_curves$grade,
+           extractioncost = A12.U_curves$extractioncost,
+           available = A12.U_curves$available ) %>%
+
+      # step 2
+      mutate(DEMO_VALUE = energy.ENERGY_DEMO_CONSTANT) ->
+
+      L112.RsrcCurves_Mt_R_U
+
+    # Realized that old step 3 wasn't correct. New behavior does xxxxxxxxx.
+    if(OLD_DATA_SYSTEM_BEHAVIOR) {
+      L112.RsrcCurves_Mt_R_U$DEMO_OOPS <- 1
+    } else {
+      L112.RsrcCurves_Mt_R_U$DEMO_OOPS <- 2
+    }
+
+    # step 4: blah blah blah
+    L112.RsrcCurves_Mt_R_U %>%
+      left_join(GCAM_region_names, by = "GCAM_region_ID") %>%
+
+    # 2b. Historical uranium prices (currently assumed at global level, so no level 1 processing necessary)
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Remove FLAG_NO_TEST when an output dataset is ready
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # If no precursors (very rare) don't call `add_precursor` at all
-    tibble() %>%
-      add_title("descriptive title of data") %>%
-      add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
-      add_comments("can be multiple lines") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_title("Uranium resource curves by GCAM region") %>%
+      add_units("Mt") %>%
+      add_comments("Currently not built up from inventory data; just using GCAM 3.0 values") %>%
+      add_comments("These were not disaggregated to regions in GCAM 3.0. Keeping this convention for now.") %>%
+      add_precursors("common/GCAM_region_names", "energy/A12.U_curves", "L113.RsrcCurves_EJ_R_MSW") %>%
       # typical flags, but there are others--see `constants.R`
-      add_legacy_name("L112.RsrcCurves_Mt_R_U_DEMO") %>%
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_legacy_name("L112.RsrcCurves_Mt_R_U_DEMO") ->
       L112.RsrcCurves_Mt_R_U_DEMO
 
     return_data(L112.RsrcCurves_Mt_R_U_DEMO)
