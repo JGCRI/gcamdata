c5d69aca3a3ad08a6af8a72d7f7161a1abcc34c1
kdorheim <kalyn.dorheim@pnnl.gov>
2017-05-04 14:00:25 -0400

    dsr demo
    The changes made to L112.U_DEMO were based on the dsr training 2 wiki
    page.

--- R/constants.R
+++ R/constants.R
@@ -163,3 +163,8 @@ emissions.TST_TO_TG <- 0.000907 # Conversion from thousand short tons to Tg
 emissions.NH3_HISTORICAL_YEARS <- 1990:2002
 emissions.NH3_EXTRA_YEARS <- 1971:1989
 emissions.EDGAR_YEARS <- 1971:2008
+
+# ======================================================================
+# Energy (demo) constants
+
+energy.DEMO_CONSTANT <- 1
