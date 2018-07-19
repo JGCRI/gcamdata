dfdaff32935dfa161c50879475bf3a67b61e33d9
Yang Ou <yango@andrew.cmu.edu>
2018-07-19 17:29:36 -0400

    code style revision

--- R/zchunk_LB174.nonghg_bld_USA.R
+++ R/zchunk_LB174.nonghg_bld_USA.R
@@ -36,29 +36,31 @@ module_gcamusa_LB174.nonghg_bld <- function(command, ...) {
 
     # Perform computations
     # This script assumes the data has been pre-processed. So all that needs to be done is
-    #to convert to the correct sector/fuel organization and convert units
+    # to convert to the correct sector/fuel organization and convert units
 
     L174.nonghg_tg_state_bld_F_Yb <- NEI_2011_GCAM_sectors %>%
-      #Subset building emissions
+      # Subset building emissions
       filter(grepl("building", GCAM_sector)) %>%
-      #Format to level1 sector naming convention
+      # Format to level1 sector naming convention
       mutate(sector = gsub("building_", "", GCAM_sector)) %>%
-      #GCAM fuels; using
-      #left_join becuase missing matching will be generated and filtered out in the next step
+      # GCAM fuels; using
+      # left_join becuase missing matching will be generated and filtered out in the next step
       left_join(CEDS_GCAM_fuel, by = "CEDS_Fuel") %>%
       rename(fuel = GCAM_fuel) %>%
-      #GCAM pollutants (filter out missing values)
+      # GCAM pollutants (filter out missing values)
       rename(NEI_pollutant = pollutant) %>%
-      #left_join becuase missing matching will be generated and filtered out in the next step
+      # left_join becuase missing matching will be generated and filtered out in the next step
       left_join(NEI_pollutant_mapping, by = "NEI_pollutant") %>%
-      #MISSING VALUES: PM filterable. Not needed bc have filt+cond. OK to omit
+      # MISSING VALUES: PM filterable. Not needed bc have filt+cond. OK to omit
       na.omit %>%
-      #Convert from short ton to Tg
+      # Convert from short ton to Tg
       mutate(emissions = emissions / CONV_T_METRIC_SHORT / 10 ^ 6, unit = "Tg") %>%
-      #Organize
+      # Organize
       group_by(state, sector, fuel, Non.CO2) %>%
       summarise(value = sum(emissions)) %>%
-      ungroup
+      ungroup %>%
+      mutate(year = 2010) %>%
+      select(state, sector, fuel, Non.CO2, year, value)
 
     L174.nonghg_tg_state_bld_F_Yb %>%
       add_title("Buildings sector non-ghg input emissions by U.S. state / sector / fuel / pollutant / year") %>%
