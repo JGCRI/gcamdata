3a1a98fed4a99bc0593f072e6858d74e0bcbad6a
Yang Ou <yango@andrew.cmu.edu>
2018-07-19 15:13:41 -0400

    create LB175 nonghg indenergy files

---
+++ R/zchunk_LB175.nonghg_indenergy_USA.R
@@ -0,0 +1,75 @@
+#' module_gcamusa_LB175.nonghg_indenergy
+#'
+#' Calculate input emissions for industrial energy use sector by input fuel in the USA.
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}: either a vector of required inputs,
+#' a vector of output names, or (if \code{command} is "MAKE") all
+#' the generated outputs: \code{L175.nonghg_tg_state_indenergy_F_Yb}. The corresponding file in the
+#' original data system was \code{LB175.nonghg_indenergy_USA.R} (gcam-usa level1).
+#' @details Industrial sector non-ghg input emissions by U.S. state / sector / fuel / pollutant / year.
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr filter mutate select
+#' @importFrom tidyr gather spread
+#' @author YO July 2018
+module_gcamusa_LB175.nonghg_indenergy <- function(command, ...) {
+  if(command == driver.DECLARE_INPUTS) {
+    return(c(FILE = "gcam-usa/gcam-usa-emission/NEI_pollutant_mapping",
+             FILE = "gcam-usa/CEDS_GCAM_fuel",
+             FILE = "gcam-usa/gcam-usa-emission/NEI_2011_GCAM_sectors"))
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return(c("L175.nonghg_tg_state_indenergy_F_Yb"))
+  } else if(command == driver.MAKE) {
+
+    # silence package check
+    GCAM_sector <- sector <- GCAM_fuel <- pollutant <-emissions <- state <-
+      sector <- fuel <- Non.CO2 <- NULL
+
+    all_data <- list(...)[[1]]
+
+    # Load required inputs
+    NEI_pollutant_mapping <- get_data(all_data, "gcam-usa/gcam-usa-emission/NEI_pollutant_mapping")
+    CEDS_GCAM_fuel <- get_data(all_data, "gcam-usa/CEDS_GCAM_fuel")
+    NEI_2011_GCAM_sectors <- get_data(all_data, "gcam-usa/gcam-usa-emission/NEI_2011_GCAM_sectors")
+
+
+    # Perform computations
+    # This script assumes the data has been pre-processed. So all that needs to be done is
+    #to convert to the correct sector/fuel organization and convert units
+
+    L175.nonghg_tg_state_indenergy_F_Yb <- NEI_2011_GCAM_sectors %>%
+      #Subset industrial energy use emissions
+      filter(GCAM_sector == "industry_energy") %>%
+      #GCAM fuels
+      left_join(CEDS_GCAM_fuel, by = "CEDS_Fuel") %>%
+      rename(fuel = GCAM_fuel) %>%
+      na.omit() %>%
+      #GCAM pollutants (filter out missing values)
+      rename(NEI_pollutant = pollutant) %>%
+      left_join(NEI_pollutant_mapping, by = "NEI_pollutant") %>%
+      ###MISSING VALUES: PM filterable. Not needed bc have filt+cond. OK to omit
+      na.omit() %>%
+      #Convert from short ton to Tg
+      mutate(emissions = emissions / CONV_T_METRIC_SHORT / 10^6, unit = "Tg") %>%
+      #Organize
+      rename(sector = GCAM_sector) %>%
+      group_by(state,sector,fuel,Non.CO2) %>%
+      summarise(emissions = sum(emissions)) %>%
+      rename(X2010 = emissions) %>%
+      ungroup()
+
+    L175.nonghg_tg_state_indenergy_F_Yb %>%
+      add_title("Industrial energy use sector non-ghg input emission factor by U.S. state / sector / fuel / pollutant / year") %>%
+      add_units("Tg") %>%
+      add_comments("Industrial energy use sector non-ghg input emission factor by U.S. state / sector / fuel / pollutant / year") %>%
+      add_legacy_name("L175.nonghg_tg_state_indenergy_F_Yb") %>%
+      add_precursors("gcam-usa/gcam-usa-emission/NEI_pollutant_mapping",
+                     "gcam-usa/CEDS_GCAM_fuel","gcam-usa/gcam-usa-emission/NEI_2011_GCAM_sectors") ->
+      L175.nonghg_tg_state_indenergy_F_Yb
+
+    return_data(L175.nonghg_tg_state_indenergy_F_Yb)
+  } else {
+    stop("Unknown command")
+  }
+}
