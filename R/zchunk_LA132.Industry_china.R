190985f44a635c22ffc7b1863f4c6a5fd32879cf
thuliuyang <15648703632@163.com>
2018-08-01 03:37:28 +0800

    finsish LA132_Industry_china

--- R/zchunk_LA132.Industry_china.R
+++ R/zchunk_LA132.Industry_china.R
@@ -30,8 +30,8 @@ module_gcam.china_LA132.Industry <- function(command, ...) {
   } else if(command == driver.MAKE) {
 
     # Silence package checks
-    fuel <- year <- province <- value <- sector <- refinery_comsumption <- GCAM_region_ID <-
-      multiplier <- NULL
+    fuel <- year <- province <- value <- sector <- GCAM_region_ID <- refinery_comsumption <- EBProcess <-
+      EBMaterial <- multiplier <- NULL
 
     all_data <- list(...)[[1]]
 
@@ -49,41 +49,66 @@ module_gcam.china_LA132.Industry <- function(command, ...) {
     # PART 1. Compute industrial energy use, removing energy used in refining
 
     # Aggregate all refining fuel consumption for electricity and gas by province
+    #TODO: skipping province industrial refining energy use for now due to units issues
+    if(1==2) {
+      #Electricity and gas inputs to refineries are now deducted from industry.
+      #Subset industrial energy use from whole-US table
     L122.in_EJ_province_refining_F %>%
       filter(fuel %in% c("electricity", "gas")) %>%
-      group_by(province, fuel, year) %>% summarise(refinery_comsumption = sum(value)) %>% ungroup ->
+      group_by(province, fuel, year) %>%
+      summarise(refinery_comsumption = sum(value)) %>%
+      ungroup ->
       L132.in_EJ_province_refining_elecgas
 
     # Adjust industrial fuel consumption by removing refinery consumption computed above
     L101.inNBS_Mtce_province_S_F %>%
       filter(sector == "industry", fuel %in% c("electricity", "gas")) %>%
+      # We need to transfer the Mtce to EJ. We haven't done that yet.
       left_join_error_no_match(L132.in_EJ_province_refining_elecgas, by = c("province", "fuel", "year")) %>%
+      #Replace any negative values with zeroes
       mutate(value = value - refinery_comsumption,
              value = if_else(value < 0, 0, value)) %>%
       select(-refinery_comsumption) ->
       L132.in_EJ_province_ind_elecgas_adj
+    }
 
-    # Bind above with other fuels considered in GCAM's "industrial energy use" sector
+    #Put together a new table for industrial energy consumption. This will be used to calculate province-wise percentages of industrial energy use
+    #Need to add biomass use by province since the CESY does not include it
+    #just using coal shares for now
+    L101.inNBS_Mtce_province_S_F %>%
+      filter(sector == "industry", fuel == "coal") %>%
+      mutate(fuel = 'biomass') ->
+      L132.in_Mtce_province_indenergy_F_unscaled.bio
+
+    # Only use the fuels that are considered in GCAM's "industrial energy use" sector
     L101.inNBS_Mtce_province_S_F %>%
       filter(sector == "industry",
-             fuel %in% L1322.in_EJ_R_indenergy_F_Yh$fuel,
-             fuel != "gas") %>%
-      bind_rows(L132.in_EJ_province_ind_elecgas_adj) ->
-      L132.in_EJ_province_indenergy_F_unscaled
+             fuel %in% L1322.in_EJ_R_indenergy_F_Yh$fuel) %>%
+      bind_rows(L132.in_Mtce_province_indenergy_F_unscaled.bio) %>%
+      complete(province, year = HISTORICAL_YEARS) %>%
+      group_by(province, sector, fuel) %>%
+      mutate(value = approx_fun(year, value, rule = 2)) %>%
+      ungroup() %>%
+      replace_na(list(value = 0)) ->
+      L132.in_Mtce_province_indenergy_F_unscaled
+
 
     # Compute fuel consumption by province and sector as proportion of china total
-    L132.in_EJ_province_indenergy_F_unscaled %>%
+    L132.in_Mtce_province_indenergy_F_unscaled %>%
       group_by(sector, fuel, year) %>%
-      mutate(value = value / sum(value)) %>% ungroup ->
+      replace_na(list(value = 0)) %>%
+      mutate(value = value / sum(value)) %>%
+      replace_na(list(value = 0)) %>%
+      ungroup ->
       L132.in_pct_province_ind_F
 
 
     # PART 2. Apportion china consumption and output to province level for non-cogeneration and cogeneration
-
     # Apportion national-level industrial energy consumption to provinces - NON-COGEN
     L132.in_pct_province_ind_F %>%
-      mutate(sector = "industry_energy") %>% rename(multiplier = value) %>%
-      # ^^ prepare for smooth join
+      mutate(sector = "industry_energy") %>%
+      rename(multiplier = value) %>%
+      # Prepare for smooth join
       left_join_error_no_match(filter(L1322.in_EJ_R_indenergy_F_Yh, GCAM_region_ID == 11),
                                by = c("fuel", "year", "sector")) %>%
       mutate(value = value *  multiplier) %>%
@@ -91,11 +116,11 @@ module_gcam.china_LA132.Industry <- function(command, ...) {
       L132.in_EJ_province_indnochp_F
 
     # Apportion national-level industrial energy consumption to provinces - COGEN
+    # Industrial sector cogeneration input energy by province and fuel
     L132.in_pct_province_ind_F %>%
-      filter(fuel %in% gcam.IND_ENERGY_USE) %>%
-      # ^^ remove fuels that are inputs to cogen systems, i.e., not electricity
+      filter(fuel %in% L123.in_EJ_R_indchp_F_Yh$fuel) %>%
+      # We only want fuels that are inputs to cogen systems, i.e. not electricity
       mutate(sector = "chp_elec") %>% rename(multiplier = value) %>%
-      # ^^ prepare for smooth join
       left_join_error_no_match(filter(L123.in_EJ_R_indchp_F_Yh, GCAM_region_ID == 11),
                                by = c("fuel", "year", "sector")) %>%
       mutate(value = value * multiplier) %>%
@@ -115,50 +140,43 @@ module_gcam.china_LA132.Industry <- function(command, ...) {
 
 
     # PART 3: Industrial feedstocks
-    # Note: Gas and liquid fuels only; each is treated separately...
-    # ... Liquid fuels are apportioned to provinces by petchem feed and asphalt;
-    # ... gas fuels are apportioned by petchem feed only.
+    # Feedstocks are the fuel inputs to the CESY Industry -- NonEnergy plus asphalt input into construction (for roads)
+    # Split province-level data into feedstocks only, according to names of fuel inputs
 
-    # Compute petroleum feedstocks by province (as proportion of china total)
+    L101.NBS_use_all_Mtce %>%
+      filter(EBProcess == "Construction" &
+               EBMaterial == "Bitumen Asphalt") %>%
+      mutate(sector = 'industry_feedstocks', fuel = "refined liquids") ->
+      L132.in_Mtce_province_indfeed_F_unscaled.road
+
     L101.inNBS_Mtce_province_S_F %>%
-      filter(sector == "industry",
-             fuel %in% c("refined liquids (const feed)", "refined liquids (petchem feed)")) %>%
-      group_by(province, sector, year) %>% summarise(value = sum(value)) %>% ungroup %>%
-      mutate(sector = "industry_feedstocks",
-             fuel = "refined liquids") %>%
+      filter(sector == "industry_feedstocks" &
+               fuel %in% unique( L1322.in_EJ_R_indfeed_F_Yh$fuel)) %>%
+      bind_rows(L132.in_Mtce_province_indfeed_F_unscaled.road[, names( L101.inNBS_Mtce_province_S_F)]) %>%
+      complete(province, year = HISTORICAL_YEARS) %>%
+      group_by(province, sector, fuel) %>%
+      mutate(value = approx_fun(year, value, rule = 2)) %>%
+      ungroup() %>%
+      replace_na(list(value = 0)) %>%
+      group_by(province, sector, fuel, year) %>%
+      summarise(value = sum(value)) %>%
+      ungroup %>%
+      replace_na(list(value = 0)) %>%
       group_by(sector, fuel, year) %>%
-      mutate(value = value / sum(value)) %>% ungroup ->
-      # ^^ computes values as proportion of china totals
-      L132.in_EJ_province_indfeed_liq_unscaled
+      mutate(value = value / sum(value)) %>%
+      ungroup %>%
+      replace_na(list(value = 0)) ->
+      L132.in_pct_province_indfeed_F
 
-    # Compute natural gas feedstocks by province (as proportion of china total)
-    # Note: assumes petrochemical feedstocks as basis for disaggregating natural gas feedstocks to provinces
-    L101.inNBS_Mtce_province_S_F %>%
-      filter(sector == "industry",
-             fuel == "refined liquids (petchem feed)") %>%
-      mutate(sector = "industry_feedstocks", fuel = "gas") %>%
-      group_by(sector, fuel, year) %>%
-      mutate(value = value / sum(value)) %>% ungroup ->
-      # ^^ computes values as proportion of china totals
-      L132.pct_province_indfeed_gas
-
-    # Replicate natural gas feedstock proportions to create coal table
-    # Note: these proportions don't actually matter, because coal feedstocks are zero for all years in china
-    L132.pct_province_indfeed_gas %>%
-      mutate(fuel = "coal") %>%
-      bind_rows(L132.pct_province_indfeed_gas) %>%
-      bind_rows(L132.in_EJ_province_indfeed_liq_unscaled) ->
-      L132.pct_province_indfeed_F
-    # ^^ proportions for petroleum, gas, and coal
 
     # Apportion feedstocks among provinces
-    L132.pct_province_indfeed_F %>%
+    L132.in_pct_province_indfeed_F %>%
       rename(multiplier = value) %>%
-      # ^^ prepare for smooth join
+      # Prepare for smooth join
       left_join_error_no_match(filter(L1322.in_EJ_R_indfeed_F_Yh, GCAM_region_ID == 11),
                                by = c("sector", "fuel", "year")) %>%
       mutate(value = value * multiplier) %>%
-      # ^^ get province portions
+      # Get province portions
       select(-multiplier, -GCAM_region_ID) %>%
       arrange(province) ->
       L132.in_EJ_province_indfeed_F
@@ -171,8 +189,9 @@ module_gcam.china_LA132.Industry <- function(command, ...) {
       add_comments("Computed by apportioning china-level consumption among provinces") %>%
       add_legacy_name("L132.in_EJ_province_indnochp_F") %>%
       add_precursors("L1322.in_EJ_R_indenergy_F_Yh",
-                     "L101.inEIA_EJ_province_S_F",
-                     "L122.in_EJ_province_refining_F") ->
+                     "L101.inNBS_Mtce_province_S_F",
+                     "L122.in_EJ_province_refining_F") %>%
+      add_flags(FLAG_PROTECT_FLOAT, FLAG_SUM_TEST) ->
       L132.in_EJ_province_indnochp_F
 
     L132.in_EJ_province_indchp_F %>%
@@ -181,7 +200,7 @@ module_gcam.china_LA132.Industry <- function(command, ...) {
       add_comments("Computed by apportioning china-level consumption among provinces") %>%
       add_legacy_name("L132.in_EJ_province_indchp_F") %>%
       add_precursors("L123.out_EJ_R_indchp_F_Yh",
-                     "L101.inEIA_EJ_province_S_F",
+                     "L101.inNBS_Mtce_province_S_F",
                      "L122.in_EJ_province_refining_F") ->
       L132.in_EJ_province_indchp_F
 
@@ -191,7 +210,7 @@ module_gcam.china_LA132.Industry <- function(command, ...) {
       add_comments("Computed by apportioning china-level CHP generation among provinces") %>%
       add_legacy_name("L132.out_EJ_province_indchp_F") %>%
       add_precursors("L123.in_EJ_R_indchp_F_Yh",
-                     "L101.inEIA_EJ_province_S_F",
+                     "L101.inNBS_Mtce_province_S_F",
                      "L122.in_EJ_province_refining_F") ->
       L132.out_EJ_province_indchp_F
 
@@ -200,7 +219,10 @@ module_gcam.china_LA132.Industry <- function(command, ...) {
       add_units("EJ") %>%
       add_comments("Computed by apportioning china-level feedstocks among provinces") %>%
       add_legacy_name("L132.in_EJ_province_indfeed_F") %>%
-      add_precursors("L101.inEIA_EJ_province_S_F", "L1322.in_EJ_R_indfeed_F_Yh") ->
+      add_precursors("L101.inNBS_Mtce_province_S_F",
+                     "L1322.in_EJ_R_indfeed_F_Yh",
+                     "L101.NBS_use_all_Mtce") %>%
+      add_flags(FLAG_PROTECT_FLOAT, FLAG_SUM_TEST) ->
       L132.in_EJ_province_indfeed_F
 
     return_data(L132.in_EJ_province_indnochp_F, L132.in_EJ_province_indchp_F, L132.out_EJ_province_indchp_F, L132.in_EJ_province_indfeed_F)
