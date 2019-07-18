d7f5a59b34c11e9638922a0be6488c81e3d703d0
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2019-07-18 12:59:04 -0400

    keep only vector form extrapolation, correct filling in all NA values error

--- R/extrapolators.R
+++ R/extrapolators.R
@@ -4,34 +4,39 @@
 #' extrapolate_constant
 #'
 #'  computes the mean of last n values and uses this constant value to fill in
-#'  missing years at the end of the time series
+#'  NA values corresponding to missing years at the end of the time series
 #'
 #' @param x Vector of values with NA's to be filled in via constant extrapolation
 #' of the mean of last n non-NA values.
 #' @param n Number of non-NA values to be averaged to provide the filler value.
 #' Defaults to n=1: using the last recorded year's value to constantly fill in
 #' the tail of vector missing values.
+#' @param numMissing The number of NA values at the tail end of each
+#' vector to be filled in. This will always be known for each data set in each
+#' chunk.
 #' @details Computes the mean of last n non-NA values of input vector x
 #' and uses this constant value to fill in NA values in x.
 #' @return Vector with all NA values replaced with the specified mean.
 #' @importFrom assertthat assert_that is.scalar
 #' @author ACS June 2019
-extrapolate_constant <- function(x, n=1){
+extrapolate_constant <- function(x, n=1, numMissing){
 
   # Some assertion tests to make sure working on right data types
   assert_that(is.numeric(x))
   assert_that(is.scalar(n))
+  assert_that(is.integer(numMissing))
+
 
   # The constant value to fill in all tail of vector NA's with.
   # = mean of the last n nonNA values in the
   meanval <- mean(last_n(x,n))
 
-  x[is.na(x)] <- meanval
+
+  # fill in only the tail end NA values with this constant.
+  x[(length(x) - numMissing + 1):length(x)] <- meanval
 
   return(x)
 }
-
-
 
 
 
