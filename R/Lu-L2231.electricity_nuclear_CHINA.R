8f0bfa1172a894c6d6644c097fabce8e0eaaf067
Ren Lu <43680471+almondtea@users.noreply.github.com>
2019-08-05 09:33:45 -0400

    Lu-L2231.electricity_nuclear_CHINA
    fix the electricity generation from nuclear by province

---
+++ R/Lu-L2231.electricity_nuclear_CHINA.R
@@ -0,0 +1,60 @@
+#' module_gcam.china_L2231.electricity_nuclear_CHINA
+#'
+#' Fixoutput of Electricity generation from nuclear by China province
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}: either a vector of required inputs,
+#' a vector of output names, or (if \code{command} is "MAKE") all
+#' the generated outputs: \code{L2231.StubTechFixOut_elec_nuclear_CHINA}. The corresponding file in the
+#' original data system was \code{L2231.electricity_nuclear_CHINA.R} (gcam-china level2).
+#' @details Fixoutput of Electricity generation from nuclear by China province from 2015 to 2030, adding StubTechnologies
+#' @importFrom dplyr mutate select rename left_join
+#' @importFrom tidyr gather
+#' @author LuRen July 2019
+module_gcam.china_L2231.electricity_nuclear_CHINA <- function(command, ...) {
+  if(command == driver.DECLARE_INPUTS) {
+    return(c(FILE = "gcam-china/province_names_mappings",
+             FILE = "gcam-china/Nuclear_incremental_fixed"))
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return(c("L2231.StubTechFixOut_elec_nuclear_CHINA"))
+  } else if(command == driver.MAKE) {
+
+
+    all_data <- list(...)[[1]]
+
+    # Silence package checks
+    province.name <- fixedOutput <- region <- supplysector <- subsector <- stub.technology <- share.weight.year <- subsector.share.weight <- share.weight <- NULL
+
+    # Load required inputs
+    province_names_mappings <- get_data(all_data, "gcam-china/province_names_mappings")
+    fixed_nuclear <- get_data(all_data, "gcam-china/Nuclear_incremental_fixed")
+
+    # ===================================================
+    # Fixoutput of Electricity generation from nuclear by China province
+
+    L223.StubTechFixOut_elec_nuclear_CHINA <- fixed_nuclear %>%
+      gather(year, fixedOutput, -province.name) %>%
+      mutate(supplysector = "electricity", subsector = "nuclear", stub.technology = "Gen_III", subsector.share.weight = 0, share.weight = 0) %>%
+      left_join(province_names_mappings %>% select(province, province.name), by = "province.name") %>%
+      mutate(year = as.numeric(substr(year, 2, 5)), share.weight.year = year) %>%
+      select(-province.name) %>%
+      rename(region = province) %>%
+      mutate(fixedOutput = if_else(fixedOutput < 0, 0, fixedOutput)) %>%
+      select(region, supplysector, subsector, stub.technology, year, fixedOutput, share.weight.year, subsector.share.weight, share.weight) %>%
+
+    # ===================================================
+    # Produce outputs
+    L2231.electricity_nuclear_CHINA() %>%
+      add_title("Electricity generation from nuclear by region/supplysector/subsector/stubtechnology") %>%
+      add_units("EJ") %>%
+      add_comments("L2231.electricity_nuclear_CHINA Fixoutput of Electricity generation from nuclear by China province") %>%
+      add_legacy_name("L223.StubTechFixOut_elec_nuclear_CHINA") %>%
+      add_precursors("gcam-china/province_names_mappings",
+                     "gcam-china/Nuclear_incremental_fixed")
+
+    return_data(L2231.StubTechFixOut_elec_nuclear_CHINA)
+  } else {
+    stop("Unknown command")
+  }
+}
