4ecc9464a281057d471a72e55f2330ff9558b765
Chris Vernon <chrisrvernon@gmail.com>
2022-06-14 10:29:50 -0400

    prep doc setup

--- coverage.R
+++
@@ -1,9 +0,0 @@
-## Script to run the coverage tests.  This is needed for running the tests on
-## travis CI because the travis_wait command mangles the arguments you need to
-## put the commands inline in the after_success section
-
-library(covr)
-Sys.setenv("gcamdata.is_coverage_test" = "TRUE")
-x <- package_coverage(function_exclusions = "^module_")
-cat("coverage = ", percent_coverage(x), " %\n")
-codecov(coverage = x)
--- tests/testthat/line_ending_files/README.md
+++
@@ -1,3 +0,0 @@
-# line_ending_files
-
-These files are used by `test_normalizer.R`.
