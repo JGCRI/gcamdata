f85f7559830e04bdf8142e7c7557e62a7f9a6fe5
russellhz <russell.horowitz@pnnl.gov>
2022-12-30 13:26:18 -0500

    L2324.Off_road currency update

--- R/zchunk_L2324.Off_road.R
+++ R/zchunk_L2324.Off_road.R
@@ -555,7 +555,7 @@ module_energy_L2324.Off_road <- function(command, ...) {
 
     L2324.GlobalTechCost_Off_road %>%
       add_title("Non-energy costs of global Off_road manufacturing technologies") %>%
-      add_units("1975$/kg for supplysector Off_road; 1975$/GJ for supplysector process heat Off_road") %>%
+      add_units(paste0(PRICE_YEAR, "$/kg for supplysector Off_road; ", PRICE_YEAR, "$/GJ for supplysector process heat Off_road")) %>%
       add_comments("For Off_road sector, the Non-energy costs of global Off_road manufacturing technologies are calculated then adjusted with CCS to include CO2 capture costs") %>%
       add_legacy_name("L2324.GlobalTechCost_Off_road") %>%
       add_precursors("energy/A324.globaltech_cost", "energy/A324.globaltech_coef") ->
